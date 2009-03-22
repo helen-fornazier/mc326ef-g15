d4a0a55bc9834c981983448ec6f4879072adc99e
Helen.Fornazier <Helen.Fornazier@c10669b8-1026-11de-b0d4-7fec019aa467>
2009-03-22 01:25:03 +0000

    Funções da biblioteca envelopadas

--- lab01-b/liblab01b.c
+++ lab01-b/liblab01b.c
@@ -10,10 +10,13 @@
 /*Definicao de palavra: Todo agrupamento formado por caracteres alfanumericos*/
 
 /*Take out signals different from letters or numbers from a string*/
-/*Substitui caracteres em remove por espacos. Nao modifica a string str. Retorna um ponteiro de uma nova string com as modificacoes*/
+/*Substitui caracteres em remove por espacos. Nao modifica a string str. Retorna um ponteiro de uma nova string com as modificacoes,
+ * ou se não houver, retorna uma cópia de str. Retorna NULL se houver falta de memória, ou se foi passado str NULL*/
 char* Cleaner(char *str, char *remove){
     char *strout=strdup(str);
-    char *temp;
+    if(strout==NULL) return NULL;
+
+    char *temp = NULL;
     while(strpbrk(strout, remove)!=NULL){
       temp=strpbrk(strout, remove);
         *temp=SPACE;
@@ -22,13 +25,19 @@ char* Cleaner(char *str, char *remove){
 }
 
 /*Take out double spacements betwen words in a string*/
-/*Substitui muitos espacos seguidos por apenas 1. Nao modifica a string str. Retorna um ponteiro de uma nova string com as modificacoes*/
+/*Substitui muitos espacos seguidos por apenas 1. Nao modifica a string str. Retorna um ponteiro de uma nova string com as modificacoes
+ * ou uma cópia de str se não houver modificações. Retorna NULL se houver falta de memória*/
 char* Corrector(char *str){
+    if(str==NULL) return NULL;
+
     int i, spaces=0;
     for(i=0;str[i]!=EOS;i++) //Conta os espacos sobrando
         if((str[i]==SPACE&&str[i+1]==SPACE) || (str[i]==SPACE&&str[i+1]==EOS)) spaces++;
+
     if(spaces){ //Se existe espaco sobrando
         char *strout=(char*)malloc(sizeof(char)*(strlen(str)-spaces+1)); //Malloca no tamanho certo, sem os espacos sobrando
+        if(strout==NULL) return NULL; //Retorna NULL se houver erro de memória
+
         int j=0;
         for(i=0;str[i]!=EOS;i++){
             if(str[i]==SPACE) continue; //Se encontrou um espaco, nao copie, continue
@@ -43,22 +52,31 @@ char* Corrector(char *str){
         else strout[j]=EOS; //Se nao, finaliza 
         return strout;
     }
-    else return strdup(str); //Se nao houver espacos sobrando, retorna a duplicata da string passada como parametro
+
+    else return strdup(str); //Se nao houver espacos sobrando, retorna a duplicata da string passada como parametro. Retorna NULL se erro de memória
 }
 
 /*Shift all small letters*/
-/*Transforma todas as letras minusculas existentes em uma string em maiusculas. Nao modifica a string str. Retorna um ponteiro de uma nova string.*/
+/*Transforma todas as letras minusculas existentes em uma string em maiusculas. Nao modifica a string str. Retorna um ponteiro de uma nova string.
+ * Retorna NULL se erro de memória*/
 char* Shifter(char *str){
+    if(str==NULL) return NULL;
+
     int i;
     char *strout=(char*)malloc(sizeof(char)*(strlen(str)+1));
+    if(strout==NULL) return NULL;
+
     for(i=0;str[i]!=EOS;i++)
         strout[i]=toupper(str[i]);
     strout[i]=EOS;
+
     return strout;
 }
 
-/*Retorna o numero de palavras em str*/
+/*Retorna o numero de palavras alfanuméricos em str*/
 int WordCount(char *str){
+    if(str==NULL) return 0;
+
     int i, words=0;
     for(i=0;str[i]!=EOS;i++){
         if(isalnum(str[i])){
@@ -70,10 +88,14 @@ int WordCount(char *str){
 }
 
 /*Conta strings em str, considerando os separadores em accept
- * Ex: str = "Bolo#De #Chocolate Congelado@"  divider = "#@"    o valor retornado é 3 strings entre separadores*/
+ * Ex: str = "Bolo#De #Chocolate Congelado@"  divider = "#@"   
+ * o valor retornado é 3 strings entre separadores*/
 int StrCount(char *str, char *divider){
+    if(str==NULL) return 0;
+
     int i, nstr=0;
     char *temp=str, *temp2=str;
+
     for(i=0;temp2!=NULL;i++){
       temp2=strpbrk(temp, divider);
         if((temp2-temp>=sizeof(char) && temp2!=NULL)){
@@ -115,13 +137,19 @@ char** Divider(char *str, int *words, char *divider){
 }
 
 /*Coloca cada pedaço de str em uma matriz retornada, os caracteres em divider são considerados separadores
- e nword sai da função com o número de strings na tabela*/
+ e nword sai da função com o número de strings na tabela. Se str for passado NULL, nword sai igual a 0 e table
+ igual a NULL. Retorna NULL se houver erro de memória*/
 char** Divider2(char* str, char* divider, int* nword){
-  *nword = StrCount(str,divider); //Conta o número de strings
-  char** table = (char**)malloc(sizeof(char)*(*nword)); //Malloca no tamanho certo
-  if(table == NULL){
-    printf("Error in function malloc Inside function Divider2\n");
+  if(str==NULL){
+      (*nword)=0;
+      return NULL;
   }
+  *nword = StrCount(str,divider); //Conta o número de strings
+  if(*nword==0) return NULL;
+
+  char** table = (char**)malloc(sizeof(char)*(*nword)); //Malloca no tamanho certo
+  if(table==NULL) return NULL;
+
   int i=0;
   
   for(i=0; i<*nword; i++){ //Iincializa table. Testado: se não inicializar antes, com muitos separadores não dá certo
@@ -144,6 +172,8 @@ char** Divider2(char* str, char* divider, int* nword){
 
 /*Retorna o numero de vezes em que o caractere c aparece em str*/
 int CountChrStr(char* str, char c){
+    if(str==NULL) return 0;
+
     int total=0;
     char* position = memchr(str,c,sizeof(char)*strlen(str));
     while(position!=NULL){
@@ -155,6 +185,8 @@ int CountChrStr(char* str, char c){
 
 /*Retorna o numero de vezes em que o caractere c aparece na matriz table de strings. nword e a quantidade de palavras presentes na tabela*/
 int CountChrTable(char** table, int nwords, char c){
+    if(table==NULL) return 0;
+
     int total = 0, i;
     for(i=0; i<nwords; i++){
         total+=CountChrStr(table[i],c);
@@ -162,8 +194,10 @@ int CountChrTable(char** table, int nwords, char c){
     return total;
 }
 
-/*Retorna o numero de vezes que a palavra word repete na tabela de palavras table. words e o numero de palavras da tabela*/
+/*Retorna o numero de vezes que a palavra word repete na tabela de palavras table. words é o numero de palavras da tabela*/
 int FindWord(char **table, int words, char *word){
+    if(table==NULL) return 0;
+
     int i, total=0;
     for(i=0;i<words;i++){
         if(!memcmp(table[i],word,sizeof(char)*(strlen(word)+1))) total++;
@@ -172,24 +206,46 @@ int FindWord(char **table, int words, char *word){
 }
 
 
-/*Cria um no com uma copia de word e com count 1*/
+/*Cria um nó com uma copia de word e com count 1, se houver algum erro de memória
+ * libera o que foi alocado e retorna NULL*/
 node* NewNode(char *word){
     node *n=(node*)malloc(sizeof(node));
+    if(n==NULL) return NULL;
+
     n->word=strdup(word);
+    if(n->word==NULL){
+        free(n);
+        return NULL;
+    }
+
     n->count=1;
     return n;
 }
 
-/*Insere a palavra word na arvore n*/
-void InsertNode(node **n, char *word){
-    if(n==NULL) n = (node**)malloc(sizeof(node*));
-    if(*n==NULL) *n=NewNode(word);
+/*Insere a palavra word na arvore n. Retorna 0 se não inseriu*/
+int InsertNode(node **n, char *word){
+    if(n==NULL){
+        n = (node**)malloc(sizeof(node*));
+        if(n==NULL) return 0;
+    }
+
+    if(*n==NULL){
+        *n=NewNode(word);
+        if((*n)==NULL) return 0;
+    }
+
+    int ver1 = 0;
+    int ver2 = 0;
+
     else{
         int i=strcmp((*n)->word,word);
         if(!i) (*n)->count++;
-        else if(i>0) InsertNode(&(*n)->left,word);
-        else if(i<0) InsertNode(&(*n)->right,word);
+        else if(i>0) ver1 = InsertNode(&(*n)->left,word);
+        else if(i<0) ver2 = InsertNode(&(*n)->right,word);
     }
+
+    if(ver1==0 && ver2==0) return 0;
+    return 1;
 }
 
 /*Imprime a arvore n na tela*/
@@ -243,12 +299,9 @@ void FreeTr(node* tree){
 /*Devolve uma cópia da string str cortada até o primeiro caractere que se encontra no divider
   Ex: str = "Bolo#de cholocate#@" divider = "#@"    devolverá a string "Bolo". Se um dos caracteres
   de divider se encontrar na primeira posição, retorna NULL, e se não houver um dos caracteres
-  retorna uma cópia de str. Se um dos parâmetros for NULL, imprime uma mensagem de erro e retorna NULL */
+  retorna uma cópia de str.*/
 char* CutStr(char* str, char* divider){
-  if(str == NULL || divider == NULL){
-    printf("Err. Funtion CutStr. Parameters maybe NULL\n");
-    return NULL;
-  }
+  if(str == NULL) return NULL;
 
   char* temp = strpbrk(str, divider);
   if(temp == NULL) return strdup(str);
@@ -265,18 +318,15 @@ char* CutStr(char* str, char* divider){
  str= “,*Cama e + Banho.” e divider=”, +*.” (o branco está depois da virgula) a rotina deve
  retornar “Cama */
 char* FirstStr(char* str, char* divider){
-if(str == NULL || divider == NULL){
-    printf("Err. Funtion CutStr. Parameters maybe NULL\n");
-    return NULL;
-  }
+    if(str == NULL) return NULL;
 
-  char* strout =CutStr(str,divider);
-  int i =0, len = strlen(str);
+    char* strout =CutStr(str,divider);
+    int i =0, len = strlen(str);
   
-  for(i=0; i<len && strout==NULL ; i++){
-     strout = CutStr(&str[i],divider);
-  }
-return strout;
+    for(i=0; i<len && strout==NULL ; i++){
+        strout = CutStr(&str[i],divider);
+     }
+    return strout;
 }
 
 /*Faz a mesma coisa que FirstStr só que leng é o índice onde se inicia srt e soma com o número de caracteres que ele andou em str.
@@ -284,10 +334,7 @@ return strout;
  * Na próxima chamada da função, len = 6, str = *“,*Cama e + Banho.”, e divider=”, +*.”, como leng inicialmente vale 6, a palavra que ele
  * irá retornar é "e" e leng = leng + 1.*/
 char* FirstStrL(char* str, char* divider, int* leng){
-    if(str == NULL || divider == NULL){
-    printf("Err. Funtion FirstStrL. Parameters maybe NULL\n");
-    return NULL;
-    }
+    if(str == NULL) return NULL;
 
     char* strout =CutStr(&str[*leng],divider);
     int i =0, len = strlen(&str[*leng]);
