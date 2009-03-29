d8b9df1b6c9dcd30500aaffe2b91e1a4d40a002a
terra0009 <terra0009@c10669b8-1026-11de-b0d4-7fec019aa467>
2009-03-29 23:05:16 +0000

    Jesuiiiis ta doido meeeeeeeeeu

--- trunk/lab02-a/LibFile.c
+++ trunk/lab02-a/LibFile.c
@@ -13,7 +13,6 @@
 //typedef char*** REGIS;
 
 int Print(FILE *f, char *c){
-    /*place error*/
     return fwrite(c,sizeof(char),strlen(c),f);
 }
 
@@ -21,9 +20,11 @@ int PrintRegister(FILE *f, char **reg, int camp){
     int n=0, i;
     char vet[100], vet2[100];
     for(i=0;i<camp;i++){
-        sprintf(vet,"%d>%s",(i+1),reg[i]);     
-        sprintf(vet2,"<%d><%s",(int)strlen(vet),vet);
-        n+=Print(f,vet2);
+        if(reg[i][0]!='\0'){
+	   sprintf(vet,"%d>%s",(i+1),reg[i]);     
+           sprintf(vet2,"<%d><%s",(int)strlen(vet),vet);
+           n+=Print(f,vet2);
+        }
     }
     return n;
 }
