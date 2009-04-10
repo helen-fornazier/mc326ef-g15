99b6f29b90809aee501216eac5b2ceead3441154
helen.fornazier <helen.fornazier@c10669b8-1026-11de-b0d4-7fec019aa467>
2009-04-10 15:34:22 +0000

    Mudança na estrutura da LibMsg, agora você precisa declarar EFILE *e;  abrir o arquivo de mensaem e = MakeMsg(e, arquivo) e para imprimir   Msg(e, 3) imprime a mensagem 3.   no final para fechar o arquivo   CloseMsg(e)

--- trunk/lab03/LibMsg.h
+++ trunk/lab03/LibMsg.h
@@ -3,16 +3,27 @@
 
 /****ABERTURA DOS ARQUIVOS DE MENSAGEM****/
 
+#include"LibWord.h"
 
-char **msg; //where will be placed all messages from the program
+typedef struct error{
+	char **msg;
+	int n;
+} EFILE;
+
+
+//char **msg; //where will be placed all messages from the program
 
 /*abre o arquivo de mensagens fname e coloca na memória
- * se não conseguiu abrir, ou se houve falta de memória, retorna 1. Se não, retorna 0*/
-int MakeMsg(char *fname);
+ * se não conseguiu abrir, ou se houve falta de memória, retorna NULL. Se não,
+ * retorna um apontador para a struct EFILE*/
+EFILE* MakeMsg(char *fname);
 
-/* imprime a mensagem de número n no arquivo de mensagens carregada por MakeMsg*/
-void Msg(int n);
+/* imprime a mensagem de número n no arquivo de mensagens carregada na struct e
+ * retornada por MakeMsg*/
+void Msg(EFILE* e, int n);
 
+/*Libera e*/
+void CloseMsg(EFILE *e);
 /****FIM DOS MANIPULADORES DOS ARQUIVOS DE MENSAGENS****/
 
 #endif /*LIB_MSG*/
