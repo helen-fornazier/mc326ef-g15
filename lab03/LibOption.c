eaef853c1ce719ccecb2f61906b54502fab7afc6
terra0009 <terra0009@c10669b8-1026-11de-b0d4-7fec019aa467>
2009-04-25 20:26:55 +0000

    mudanca da opcao 11 e 12

--- lab03/LibOption.c
+++ lab03/LibOption.c
@@ -534,6 +534,12 @@ DATASTYLE *Option10(EFILE *e, DATASTYLE *data){
 }
 
 
+
+
+
+
+/******************
+
 void Option11(EFILE *e, DATASTYLE *search, DATASTYLE *find){
 	char sname[TAMS], fname[TAMS], key[TMS];
 	FILE *sf, *ff;
@@ -590,6 +596,83 @@ void Option12(EFILE *e, DATASTYLE *config){
 	}
 	fclose(f);
 }
+
+******************/
+
+
+
+void Option11(EFILE e, DATASTYLE *search, DATASTYLE *find){
+	char sname[100], fname[100], key[50];
+	FILE *fs, *ff;
+	int i;
+	char **register;
+
+	msg(e,20);
+	scanf("%s",sname);
+	fs=fopen(sname,"r");
+	if(fs==NULL){
+		msg(e,11);
+		return;
+	}
+	msg(e,21);
+	scanf("%s",fname);
+	ff=fopen(fname,"r");
+	if(ff==NULL){
+		fclose(fs);
+		msg(e,11);
+		return;
+	}
+	msg(e,13);
+	scanf("%s",key);
+
+	i=BinaryKeySearch(sf,ff,search,find,key);
+	if(!i){
+		msg(e,15);
+	}
+	
+			ReadRegVar(ff,&rregister,find->nfield);
+			if(rregister[0][0]=='s'){
+				PrintOne(find,rregister,1);
+				
+			}
+
+			if(rregister[0][0]=='n'){
+					
+				msg(e,15);
+			}
+			FreeT(register,find->nfield);	
+
+	fclose(fs);
+	fclose(ff);
+}
+
+
+void Option12(EFILE e, DATASTYLE config){
+char fname[100], key[50];
+int i;
+
+	msg(e,9);
+	scanf("%s",fname);
+	f=fopen(sname,"r");
+	if(f==NULL){
+		msg(e,11);
+		return;
+	}
+	
+	msg(e,22);
+	scanf("%s",key);
+
+	i=EraseReg(f,key,config->nfield);
+	if(i){
+		msg(e,23);
+	}
+	else{
+		msg(e,15);
+	}
+	fclose(f);
+}
+
+
 
 
 
