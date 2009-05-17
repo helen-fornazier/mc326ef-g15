9d4e82d6a82f3b83082978ddfaa1201537866df1
terra0009 <terra0009@c10669b8-1026-11de-b0d4-7fec019aa467>
2009-05-17 02:33:18 +0000

    inicio do lab04 (quicksort)

--- pirata.c
+++ pirata.c
@@ -1,31 +1,51 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
 
-int Print(FILE *f, char *c){
-    /*place error*/
-    return fwrite(c,sizeof(char),strlen(c),f);
+int PrintRegister(file *f,char **reg, int nfields){
+	int i, n;
+	n=0;
+	for(i=0;i<nfields;i++)
+		if(*reg[i]!='\0'){
+			fprintf(f,"%c%c%s",(char)(strlen(reg[i])+2),(char)i,vet[i]);
+			n+=strlen(reg[i])+2;
+		}
+	return n;
 }
 
-int PrintRegister(FILE *f, char **reg, int camp){
-    int n=0, i;
-    char vet[100], vet2[100];
-    for(i=0;i<camp;i++){
-        sprintf(vet,"%d>%s",(i+1),reg[i]);     
-        sprintf(vet2,"<%d><%s",(int)strlen(vet),vet);
-        n+=Print(f,vet2);
-    }
-    return n;
+int ReadField(file *f, char *vet, char sep){
+	int size, field;
+
+	if(vet!=NULL) free(vet);
+	
+	fscanf(f,"%c",&(char)size);
+	if(size==(int)sep || (char)size==EOF) return 0;
+	fscanf(f,"%c",&(char)field);
+	vet=(char*)malloc(sizeof(char)*(size-2));
+	fread(vet,sizeof(char),size-2,f);
+
+	return field;
 }
 
-int PrintAll(FILE *f, int type, char ***reg, int treg, char end, int camp){
-    int n=0,i;
-    char vet[3];
-    sprintf(vet,"%c\n",end);
-    for(i=0;i<treg;i++){
-        if(type) n+=PrintRegister(f,reg[i],camp);
-        else n+=PrintStrDiv(f,reg[i],camp,"|");
-        n+=Print(f,vet);
-    }
-    return n;
+long int ReadRegister(file *f, char ***reg, int fields, char sep){
+	int n, i=1;
+	char *vet;
+	long int start=ftell(f);
+
+	if(reg!=NULL) printf("reading function receives non null char***\n");
+	*reg=(char**)malloc(sizeof(char*)*fields);
+	while(1){
+		n=ReadField(f,vet,sep);
+		
+		if(n=0) break;
+		
+		if(n>fields) printf("number of fields in file is greater than expected \n");
+		for(;i<n;i++){
+			(*reg)[i-1]=(char*)malloc(sizeof(char));
+			(*reg)[i-1][0]='\0';
+		}
+		(*reg)[i-1]=strdup(vet);
+		free(vet);
+		i++;
+	}
+	
+	if(i==1) return 0;
+	return start;
 }
