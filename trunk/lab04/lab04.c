a92346cfb569df2dac11c520452869fad8536d68
terra0009 <terra0009@c10669b8-1026-11de-b0d4-7fec019aa467>
2009-05-18 01:52:13 +0000

    Continuacão lab04

--- trunk/lab04/lab04.c
+++ trunk/lab04/lab04.c
@@ -1,59 +1,37 @@
 #define DATA "data.config"
 #include<time.h>
+#include"LibFile.h"
+#include"LibWord.h"
+#include"LibMsg.h"
+
+
+
 
 typedef struct information{
-	int write, read, file;
+	int write, read, file, merge;
 	double time;
-} INFORMATION
+} INFORMATION;
 
-void quickSort(int i, int j, REGIS vet, int field){
-	if(i>=j) return;
-	int esq=i, dir = j;
-	char **aux = vet[(i+j)/2], **aux2;
-	int temp;
-
-	while(1){
-		while(1){
-			temp=strcmp(aux[field],vet[esq][field]);
-			if(temp>0) esq++;
-			else break;
-		}
-		while(1){
-			temp=strcmp(vet[dir][field],aux[field]);
-			if(temp>0) dir--;
-			else break;
-		}
-		if(esq<=dir){
-			aux2=vet[esq];
-			vet[esq]=vet[dir];
-			vet[dir]=aux2;
-			esq++; dir--;
-		}
-		else break;
-	}
-	quickSort(i,dir,vet,field);
-	quickSort(esq,j,vet,field);
-}
 
 int FirstOrder(FILE *f, int field, int memory, DATASTYLE *ds){
 	int regsize=0,i;
 	for(i=0;i<ds->nfield;i++) regsize+=ds->efield[i];
-	int nreg=memory/(regize+1), readreg=nreg, nfile=0;
+	int nreg=memory/(regsize+1), readreg=nreg, nfile=0;
 	REGIS matrix;
-	char tempstr[TAMS];
+	char tempstr[TAMS]; 
 	FILE *out;
 	char *str;
 	
 	system("mkdir tempfolder");
 	while(readreg==nreg){
-		readreg=Div(f,&matrix,ds->efield,ds->nfields,nreg);
+		readreg=Div(f,&matrix,ds->efield,ds->nfield,nreg);
 		if(readreg==0) break;
 		quickSort(0,nreg-1,matrix,field);
 		nfile++;
 		sprintf(tempstr,"tempfolder/%d",nfile);
 		out=fopen(tempstr,"w");
 		TabletoStr(&str,matrix,readreg,regsize,ds->nfield);
-		fwrite(str,sizeof(char),(regsize+1)*readreg);
+		fwrite(str,sizeof(char),(regsize+1)*readreg,out);
 		FreeTT(matrix,readreg,ds->nfield);
 		free(str);
 		fclose(out);
@@ -64,13 +42,13 @@ int FirstOrder(FILE *f, int field, int memory, DATASTYLE *ds){
 int MergeSort(int nfile, int field, int memory, DATASTYLE *ds, INFORMATION *info){
 	int regsize=0, i;
 	for(i=0;i<ds->nfield;i++) regsize+=ds->efield[i];
-	int read=0, write=0, nreg=memory/(3*regsize), nread, nfilea, newfile ;
-	int readrega, readregb, i, j, k, temp;
+	int nreg=memory/(3*regsize), nfilea, newfile ;
+	int readrega, readregb, j, k, temp;
 	FILE *a, *b, *c;
 	char tempvet[TAMS];
-	REGIS ra, rb, rc=(REGIS)malloc(sizeof(char**)*nreg), tempr;
-	int re=0,wr=0,creatfile=0;
-	char *str;
+	REGIS ra, rb, rc=(REGIS)malloc(sizeof(char**)*nreg);
+	int re=0,wr=0;
+	char *str=NULL;
 
 	if(nfile<=1) return 1;
 
@@ -83,19 +61,19 @@ int MergeSort(int nfile, int field, int memory, DATASTYLE *ds, INFORMATION *info
 	newfile=nfile+1;
 	sprintf(tempvet,"tempfolder/%d",newfile);
 	c=fopen(tempvet,"w");
-	readrega=Div(a,&ra,ds->efield,ds->nfields,nreg);
+	readrega=Div(a,&ra,ds->efield,ds->nfield,nreg);
 	re++;
-	readregb=Div(b,&rb,ds->efield,ds->nfields,nreg);
+	readregb=Div(b,&rb,ds->efield,ds->nfield,nreg);
 	re++;
-	while(){
-		i=j=k=0
-		while(){
+	while(1){
+		i=j=k=0;
+		while(1){
 			temp=strcmp(ra[i][field],rb[j][field]);
 			if(temp<0){
 				rc[k++]=ra[i++];
 				if(i==readrega){
-					FreeTT(readrega,ds->nfield);
-					readrega=Div(a,&ra,ds->efield,ds->nfields,nreg);
+					FreeTT(ra,readrega,ds->nfield);
+					readrega=Div(a,&ra,ds->efield,ds->nfield,nreg);
 					re++;
 					i=0;
 				}
@@ -103,8 +81,8 @@ int MergeSort(int nfile, int field, int memory, DATASTYLE *ds, INFORMATION *info
 			else{
 				rc[k++]=rb[j++];
 				if(j==readregb){ 
-					FreeTT(readregb,ds->nfield);
-					readregb=Div(b,&rb,ds->efield,ds->nfields,nreg); 
+					FreeTT(rb,readregb,ds->nfield);
+					readregb=Div(b,&rb,ds->efield,ds->nfield,nreg); 
 					re++;
 					j=0;
 				}
@@ -123,13 +101,13 @@ int MergeSort(int nfile, int field, int memory, DATASTYLE *ds, INFORMATION *info
 				wr++;
 		
 				TabletoStr(&str,&rb[j],(readregb-j),regsize,ds->nfield);
-				fwrite(str,sizeof(char),(regsize+1)*(readregb-j));
+				fwrite(str,sizeof(char),(regsize+1)*(readregb-j),c);
 				free(str);
 				wr++;
 				
 				while(!feof(b)){
 					/*le memory em b*/
-					readregb=Read(&str,regsize,nreg*3,b);
+					readregb=Read(b,&str,regsize,nreg*3);
 					fwrite(str,sizeof(char),readregb*(regsize+1),c);
 					free(str);
 					wr++;
@@ -143,12 +121,12 @@ int MergeSort(int nfile, int field, int memory, DATASTYLE *ds, INFORMATION *info
 	
 				wr++;
 				TabletoStr(&str,&ra[j],(readrega-i),regsize,ds->nfield);
-				fwrite(str,sizeof(char),(regsize+1)*(readrega-i));
-				free(str)
+				fwrite(str,sizeof(char),(regsize+1)*(readrega-i),c);
+				free(str);
 	
 				wr++;
 				while(!feof(a)){
-					readregb=Read(&str,regsize,nreg*3,a);
+					readregb=Read(a,&str,regsize,nreg*3);
 					fwrite(str,sizeof(char),readregb*(regsize+1),c);
 					free(str);
 	
@@ -158,7 +136,7 @@ int MergeSort(int nfile, int field, int memory, DATASTYLE *ds, INFORMATION *info
 			}
 		}
 	
-		seek(c,0,SEEK_SET);
+		fseek(c,0,SEEK_SET);
 		fclose(a);
 		fclose(b);
 		a=c;
@@ -171,10 +149,10 @@ int MergeSort(int nfile, int field, int memory, DATASTYLE *ds, INFORMATION *info
 		b=fopen(tempvet,"r");
 		newfile++;
 		c=fopen(tempvet,"w");
-		readrega=Div(a,&ra,ds->efield,ds->nfields,nreg);
+		readrega=Div(a,&ra,ds->efield,ds->nfield,nreg);
 
 		re++;
-		readregb=Div(b,&rb,ds->efield,ds->nfields,nreg);
+		readregb=Div(b,&rb,ds->efield,ds->nfield,nreg);
  
 		re++;
 	}
@@ -191,12 +169,13 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 	
-	FILE f = foppen(DATA,r);
+	FILE *f = fopen(DATA,"r");
 	DATASTYLE *data=FillData(f);
 	fclose(f);
-	f=fopen(argv[1],r);
+	f=fopen(argv[1],"r");
 	int memory=atoi(argv[3]), temp, field;
 	INFORMATION *info=(INFORMATION*)malloc(sizeof(INFORMATION));
+	char str[TAMS];
 
 	printf("\ndigite 0 se quizer ordenado por RA ou 1 por Nome\n");
 	scanf("%d",&field);
@@ -207,9 +186,14 @@ int main(int argc, char *argv[]){
 	temp=FirstOrder(f,field,memory,data);
 	fclose(f);
 	MergeSort(temp,field,memory,data,info);
+	
+	sprintf(str,"mv tempfolder/%d %s",info->file,argv[2]);
+	system(str);
+	system("rm tempfolder");
 
 	time(&end);
-	info->time=diftime(end,start);
+	info->time=difftime(end,start);
+	info->merge=info->file-temp;
 
 	return 0;
 }
